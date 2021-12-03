9dd698b1c7b528435d3c3496e23823df825ec83e
jhairssteven <71862429+jhairssteven@users.noreply.github.com>
2021-12-02 20:17:33 -0500

    API gateway clean-up

--- API-GATEWAY/src/typeDefs/image_type_defs.js
+++ API-GATEWAY/src/typeDefs/image_type_defs.js
@@ -1,14 +1,16 @@
 const {gql} = require('apollo-server');
 
-const imageTypeDefs = gql`
+const imageTypeDefs = gql `
   type Image{
       id: String!
       postIdDestiny: String!
-      imagen: String!(o que tipo de dato sera esto?)
+      imagen: String!
+      // (o que tipo de dato sera esto?)
   }
   input ImageInput{
       postIdDestiny: String!
-      image: Binary!(o que tipo de dato sera esto? o MultipartFile?)
+      image: Binary!
+      // (o que tipo de dato sera esto? o MultipartFile?)
   }
 
   extend type Query{
--- API-GATEWAY/src/typeDefs/index.js
+++ API-GATEWAY/src/typeDefs/index.js
@@ -1,11 +1,12 @@
-const postTypeDefs = require('./post_type_defs')
-const likeTypeDefs = require('./like_type_defs')
-const commentTypeDefs = require('./comment_type_defs')
-const imageTypeDefs = require('./image_type_defs')
+// const postTypeDefs = require('./post_type_defs')
+// const likeTypeDefs = require('./like_type_defs')
+// const commentTypeDefs = require('./comment_type_defs')
+// const imageTypeDefs = require('./image_type_defs')
 const messageTypeDefs = require("./message_type_defs")
 const userTypeDefs = require('./user_type_defs')
 
-const schemasArrys = [userTypeDefs, postTypeDefs, likeTypeDefs, 
-            commentTypeDefs, imageTypeDefs, messageTypeDefs ];
+// const schemasArrys = [userTypeDefs, postTypeDefs, likeTypeDefs, 
+//             commentTypeDefs, imageTypeDefs, messageTypeDefs ];
+const schemasArrys = [userTypeDefs, messageTypeDefs ];
 
 module.exports = schemasArrys;
