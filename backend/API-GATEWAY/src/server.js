6e4b42ec73b0e337012c5ed6adc48197bc76ee77
“difsan” <difsanchezru@gmail.com>
2021-12-02 19:51:56 -0500

    se modifica el user-ms y se agregan avances del api de post

--- backend/API-GATEWAY/src/server.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    user_profile_api_url: 'https://mintagram-user-profile-ms.herokuapp.com',
-    posts_api_url: 'https://minta-messages-ms.herokuapp.com',
-    message_api_url: 'https://min-post-ms.herokuapp.com',
-    };
